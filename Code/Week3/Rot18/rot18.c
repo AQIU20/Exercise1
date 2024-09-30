1309212a51363f36d360703c3f5e1a7b27d27c6a
AQIU20 <aqiu0920@gmail.com>
2024-09-30 08:27:19 +0800

    rot18

--- Code/Week3/Rot18/rot18.c
+++ Code/Week3/Rot18/rot18.c
@@ -1,11 +1,37 @@
 #include "rot18.h"
 
 void rot(char str[]){
-    // your code here
+    int i=0;
+
+    while(str[i]!='\0'){
+        if(str[i]>='a' && str[i]<='z'){
+            str[i] =(str[i] - 'a' + 13) % 26 + 'a';
+        }
+
+        if(str[i]>='A' && str[i]<='Z'){
+            str[i] =(str[i] - 'A' + 13) % 26 + 'A';
+        }
+
+        if(str[i]>='0' && str[i]<='9'){
+            str[i] =(str[i] - '0' + 5) % 10 + '0';
+        }
+        i++;
+
+    }
+
 }
 
-// any other functions here
+
 
 void your_tests(void){
-    // your tests here
+    char str[100];
+
+    //Border situation
+
+    strcpy(str, "Az09");
+    rot(str);
+    assert(strcmp(str, "Nm54") == 0);
+    printf("Success\n");
+
+    
 }
