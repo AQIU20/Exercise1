1a3b09c60a744cfb719100babb7cc05da40bbf06
AQIU20 <aqiu0920@gmail.com>
2024-10-02 21:40:41 +0800

    rot18 final

--- Code/Week3/Rot18/rot18.c
+++ Code/Week3/Rot18/rot18.c
@@ -3,20 +3,25 @@
 void rot(char str[]){
     int i=0;
 
+    if (str == NULL) {
+        fprintf(stderr, "Please enter\n");
+        exit(EXIT_FAILURE);
+    }
+
     while(str[i]!='\0'){
+        //a-z
         if(str[i]>='a' && str[i]<='z'){
             str[i] =(str[i] - 'a' + 13) % 26 + 'a';
         }
-
+        //A-Z
         if(str[i]>='A' && str[i]<='Z'){
             str[i] =(str[i] - 'A' + 13) % 26 + 'A';
         }
-
+        //0-9
         if(str[i]>='0' && str[i]<='9'){
             str[i] =(str[i] - '0' + 5) % 10 + '0';
         }
         i++;
-
     }
 
 }
@@ -27,11 +32,9 @@ void your_tests(void){
     char str[100];
 
     //Border situation
-
     strcpy(str, "Az09");
     rot(str);
     assert(strcmp(str, "Nm54") == 0);
-    printf("Success\n");
 
     
 }
